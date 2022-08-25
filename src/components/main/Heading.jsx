46490d9a9874cff42f4597989afb1c1c5406bee6
jnrlns <llanesjorge262@gmail.com>
2022-08-24 21:26:30 -0500

    Finished menu & Mobile res

--- src/components/main/Heading.jsx
+++ src/components/main/Heading.jsx
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 // import earth from "../../images/earth.jpeg"
-import { HashLink as Link} from "react-router-hash-link";
+import { HashLink as Link } from "react-router-hash-link";
 import "./heading.css";
 
 function Heading() {
+  const [show, setShow] = useState(false);
+  const untoggleMenu = () => {
+    setShow(false);
+  };
   return (
     <div>
       <section className="heading-container" id="home">
+        {show ? (
+          <nav className="MR-NAV ">
+            <Link
+              className="linkss link1"
+              to="#home"
+              spy="true"
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={untoggleMenu}>
+              Home
+            </Link>
+            <Link
+              className="linkss link2"
+              to="#projects"
+              spy="true"
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={untoggleMenu}>
+              Projects
+            </Link>
+            <Link
+              className="linkss link3"
+              to="#about"
+              spy="true"
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={untoggleMenu}>
+              About
+            </Link>
+            <Link
+              className="linkss link4"
+              to="#contact"
+              spy="true"
+              smooth={true}
+              offset={50}
+              duration={500}
+              onClick={untoggleMenu}>
+              Contacts
+            </Link>
+          </nav>
+        ) : null}
+        <a href="##" className="toggle-button" onClick={() => setShow(!show)}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
         <div className="name-nav">
           <div>
             <h1 className="first-name">JORGE</h1>
@@ -15,16 +68,40 @@ function Heading() {
           </div>
           <div className="nav-container">
             <nav className="nav">
-              <Link className="links" to="#home" spy="true" smooth={true} offset={50} duration={500}>
+              <Link
+                className="links"
+                to="#home"
+                spy="true"
+                smooth={true}
+                offset={50}
+                duration={500}>
                 Home
               </Link>
-              <Link className="links" to="#projects" spy="true" smooth={true} offset={50} duration={500}>
+              <Link
+                className="links"
+                to="#projects"
+                spy="true"
+                smooth={true}
+                offset={50}
+                duration={500}>
                 Projects
               </Link>
-              <Link className="links" to="#about" spy="true" smooth={true} offset={50} duration={500}>
+              <Link
+                className="links"
+                to="#about"
+                spy="true"
+                smooth={true}
+                offset={50}
+                duration={500}>
                 About
               </Link>
-              <Link className="links" to="#contact" spy="true" smooth={true} offset={50} duration={500}>
+              <Link
+                className="links"
+                to="#contact"
+                spy="true"
+                smooth={true}
+                offset={50}
+                duration={500}>
                 Contacts
               </Link>
             </nav>
